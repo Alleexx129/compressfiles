d283b1dea3a8a1762016c69aea4944fe9ca87517
Alleexx129 <132962983+Alleexx129@users.noreply.github.com>
2023-05-09 07:02:57 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_package',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas'
+    ],
+    author='Alleexx',
+    author_email='Alleexx129@gmail.com',
+    description='A description of your package',
+    url='https://github.com/Alleexx129/txt-compressor/new/main',
+    license='MIT',
+)

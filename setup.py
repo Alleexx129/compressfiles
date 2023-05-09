5f690e2b013bdb800cfdebca981aa84c2ce698d6
Alleexx129 <132962983+Alleexx129@users.noreply.github.com>
2023-05-09 12:30:10 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_package',
+    name='txt-compressor',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
@@ -13,7 +13,7 @@ setup(
     ],
     author='Alleexx',
     author_email='Alleexx129@gmail.com',
-    description='A description of your package',
+    description='A txt file compressor',
     url='https://github.com/Alleexx129/txt-compressor/new/main',
     license='MIT',
 )

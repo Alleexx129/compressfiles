e8900c7095af6690cd3612be5aba1e05770acae3
Alleexx129 <132962983+Alleexx129@users.noreply.github.com>
2023-05-24 12:55:17 -0400

    finalized for upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='txt-compressor',
-    version='0.1.0',
+    VERSION='0.1.0',
     packages=find_packages(),
     install_requires=[
         'numpy',
@@ -11,9 +11,9 @@ setup(
         'typing',
         'tqdm'
     ],
-    author='Alleexx',
-    author_email='Alleexx129@gmail.com',
-    description='A txt file compressor',
-    url='https://github.com/Alleexx129/txt-compressor/new/main',
+    LONG_DESCRIPTION='A small library to compress txt files using gzip',
+    DESCRIPTION='compress txt files',
+    keywords=['python', 'txt', 'compress', 'txt-compressor']
+     url='https://github.com/Alleexx129/txt-compressor/new/main',
     license='MIT',
 )
